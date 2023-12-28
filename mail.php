64e2d24514540640c49797e13a86da6403a802bf
Nurvive <jiimbo689@gmail.com>
2023-12-28 18:47:16 +0300

    release

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+/* Здесь проверяется существование переменных */
+if (isset($_POST['name'])) {$name = $_POST['name'];}
+if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+ 
+/* Сюда впишите свою эл. почту */
+$myaddres  = "vat64279@zslsz.com"; // кому отправляем
+ 
+/* А здесь прописывается текст сообщения, \n - перенос строки */
+$mes = "Тема: Заказ обратного звонка!\nТелефон: $phone\nИмя: $name";
+ 
+/* А эта функция как раз занимается отправкой письма на указанный вами email */
+$sub='Заказ'; //сабж
+$email='Заказ обратного звонка'; // от кого
+$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+ini_set('short_open_tag', 'On');
+header('Refresh: 1; URL=index.html');
+?>
